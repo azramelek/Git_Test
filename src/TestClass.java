7ec996dbbfc8e7184f1336cde9135961de512a81
azramelek <67170272+azramelek@users.noreply.github.com>
2020-06-19 16:59:57 -0500

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
-       System.out.println("Git den mehaba!");
+       System.out.println("Git den mehaba Azra!");
+       System.out.println("GitHub den mehaba Azra!");
     }
 }
